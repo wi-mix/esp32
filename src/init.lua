2ff6cc82be0a008726f09592333a176a9fd1a075
David Skrundz <david@skrundz.ca>
2018-02-25 01:41:54 -0700

    Start uart. Also add halt() command to not crash

--- src/init.lua
+++ src/init.lua
@@ -5,4 +5,11 @@ local wait = tmr.create()
 wait:alarm(2000, tmr.ALARM_SINGLE, function()
   dofile("Main.lc")
 end)
+
+function halt()
+  wait:unregister()
+  wait = nil
+end
+
 print("Waiting 2 seconds before starting...")
+print("run `halt()` to stop")
