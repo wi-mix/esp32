da18b78fc983b37c72d5507d3edfe66c31d63ffd
David Skrundz <david@skrundz.ca>
2018-03-05 12:34:20 -0700

    use the string builder and buffer

--- src/Main.lua
+++ src/Main.lua
@@ -15,7 +15,31 @@ end)
 LED.init()
 LED.off()
 
-sendQueue = Queue.new()
+contentIndex = 6
+contentLengthIndex = 4
+httpResponse = StringFormat.new()
+httpResponse[1] = "HTTP/1.1 200 OK\n"
+httpResponse[2] = "Content-Type: text/html\n"
+httpResponse[3] = "Content-Length: "
+httpResponse[4] = 0
+httpResponse[5] = "\n\n"
+httpResponse[6] = ""
+
+htmlIndex = 7
+html = StringFormat.new()
+html[ 1] = "<html>"
+html[ 2] = "<head>"
+html[ 3] = "</head>"
+html[ 4] = "<body>"
+html[ 5] = "<center>"
+html[ 6] = "<h1>"
+html[ 7] = ""
+html[ 8] = "</h1>"
+html[ 9] = "</center>"
+html[10] = "</body>"
+html[11] = "</html>"
+
+sendBuffer = SendBuffer.new()
 
 w = Wifi.init()
 s = nil
@@ -37,10 +61,15 @@ w:onGetIp(function(ip, netmask, gw)
   s:listen(CONST.port, function(c)
     c:onReceive(function(c, data)
       print("-- Receive data: " .. data)
-      c:send("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 75\n\n<html><head></head><body><center><h1>Lua Server</h1></center></body></html>")
+      html[htmlIndex] = "HTML SERVER"
+      response = html:render()
+      httpResponse[contentIndex] = response
+      httpResponse[contentLengthIndex] = response:len()
+      sendBuffer:send(c, httpResponse:render())
     end)
     c:onSent(function(client)
       print("-- Done sending")
+      sendBuffer:sent()
     end)
     local port, ip = c:address()
     print("-- Client connected to " .. ip .. ":" .. port)
