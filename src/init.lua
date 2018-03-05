77fb1831abf2be4b762f7d725e0ba567ba86ccaa
David Skrundz <david@skrundz.ca>
2018-03-05 12:43:07 -0700

    move print to the top

--- src/init.lua
+++ src/init.lua
@@ -1,6 +1,9 @@
 -- init.lua
 -- David Skrundz
 
+print("Waiting 2 seconds before starting...")
+print("run `halt()` to stop")
+
 local wait = tmr.create()
 wait:alarm(2000, tmr.ALARM_SINGLE, function()
   dofile("Main.lc")
@@ -10,6 +13,3 @@ function halt()
   wait:unregister()
   wait = nil
 end
-
-print("Waiting 2 seconds before starting...")
-print("run `halt()` to stop")
